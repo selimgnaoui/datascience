4a33d9ffc1b25e5c07ce9192ac9545d31da44e4b
gnaouis47 <selim.gnaoui@gmail.com>
2017-11-08 23:32:51 +0100

    ADMIN MODE

---
+++ write_twets.py
@@ -0,0 +1,19 @@
+import pymongo
+class Write_Tweest(object):
+    db=""
+    def __init__(self):
+      
+      connection = pymongo.MongoClient('ds237475.mlab.com',37475)
+      self.db = connection['test2']
+      self.db.authenticate('aaaaa', 'aaaaa')
+       
+    def savetodb(self,tweets):
+       (self.db.us.insert_many(tweets))
+    
+    def showtweets(self):
+       cursor =self.db.us.find({})
+       for document in cursor:
+          print(document)
+        
+    def deletetweets(self):
+         self.db.us.delete_many({})
